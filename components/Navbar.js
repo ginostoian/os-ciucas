3bbef5771bf858b84dae918cd7069ce8e11acb13
Gino Stoian <ginostoian@gmail.com>
2022-10-29 02:43:02 +0300

    added logo to navigation

--- components/Navbar.js
+++ components/Navbar.js
@@ -1,8 +1,26 @@
+import Link from 'next/link'
+import Head from 'next/head'
+import Image from 'next/image'
+
+import classes from './Navbar.module.css'
+
 function Navbar() {
     return (
-        <div>
-            This is the nav
-        </div>
+        <nav>
+            <div className={classes.logoContainer}>
+                <Link href='/'>
+                    <Image src='/images/os-ciucas-logo.png' alt='Logo Ocol Silvic Ciucas' width='120' height='120' />
+                </Link>
+            </div>
+
+            <div className={classes.linksContainer}>
+                <ul>
+                    <li>
+
+                    </li>
+                </ul>
+            </div>
+        </nav>
     )
 }
 
