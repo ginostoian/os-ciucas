3c7c37b1bae38ff1924862dd11cdeead1ff9bca1
Gino Stoian <ginostoian@gmail.com>
2022-11-08 01:47:25 +0200

    add logic to form to collect data and console it

--- components/contact/ContactForm.js
+++ components/contact/ContactForm.js
@@ -7,20 +7,63 @@ const poppins = Poppins({ weight: '400' })
 const poppinsBold = Poppins({ weight: '700' })
 
 const ContactForm = () => {
+    const [name, setName] = useState('')
+    const [email, setEmail] = useState('')
+    const [phone, setPhone] = useState('')
+    const [message, setMessage] = useState('')
+
+    const onNameChange = (e) => {
+        setName(e.target.value)
+    }
+
+    const onEmailChange = (e) => {
+        setEmail(e.target.value)
+    }
+
+    const onPhoneChange = (e) => {
+        setPhone(e.target.value)
+    }
+
+    const onMessageChange = (e) => {
+        setMessage(e.target.value)
+    }
+
+    const onFormSubmit = (e) => {
+        e.preventDefault()
+        const formData = {}
+
+        if (name) {
+            formData.name = name
+        }
+        if (email) {
+            formData.email = email
+        }
+        if (phone) {
+            formData.phone = phone
+        }
+        if (message) {
+            formData.message = message
+        }
+        setName('')
+        setEmail('')
+        setPhone('')
+        setMessage('')
+        console.log(formData)
+    }
     return (
         <section className={`${classes.container} ${poppins.className}`}>
             <h2 className={`${classes.title} ${poppinsBold.className}`}>Scrie-ne</h2>
 
-            <form className={classes.formContainer}>
+            <form onSubmit={onFormSubmit} className={classes.formContainer}>
                 <div className={classes.formInputs}>
                     <div className={classes.formLeft}>
-                        <input type="text" placeholder='Nume*' required />
-                        <input type="email" placeholder='Email*' required />
-                        <input type="number" placeholder='Telefon*' required />
+                        <input onChange={onNameChange} type="text" placeholder='Nume*' value={name} required />
+                        <input onChange={onEmailChange} type="email" placeholder='Email*' value={email} required />
+                        <input onChange={onPhoneChange} type="number" placeholder='Telefon*' value={phone} required />
                     </div>
 
                     <div className={classes.formRight}>
-                        <textarea name="mesaj" placeholder="Mesaj" id="mesaj" className={classes.mesaj} cols="30" rows="10" required>
+                        <textarea onChange={onMessageChange} value={message} name="mesaj" placeholder="Mesaj" id="mesaj" className={classes.mesaj} cols="30" rows="10" required>
 
                         </textarea>
                     </div>
