c12219e113919b2d9212d294d4c3a9458507a723
Gino Stoian <ginostoian@gmail.com>
2022-11-14 23:02:00 +0200

    add firebase config

---
+++ firebase.js
@@ -0,0 +1,17 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyCn5S6O_00T-LnzlH4bReqF2fGZ0IIcHgM",
+    authDomain: "os-ciucas.firebaseapp.com",
+    projectId: "os-ciucas",
+    storageBucket: "os-ciucas.appspot.com",
+    messagingSenderId: "728474487656",
+    appId: "1:728474487656:web:b161e610a2e7d33474e4dd"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
