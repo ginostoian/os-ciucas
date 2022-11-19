1ca437cb515cd5f575070ee8c5da477c6cfb9f09
Gino Stoian <ginostoian@gmail.com>
2022-11-20 01:41:01 +0200

    fetch announcements from firestore and display them

--- components/anunturi/Anunt.js
+++ components/anunturi/Anunt.js
@@ -6,13 +6,13 @@ import classes from './Anunt.module.css'
 const poppins = Poppins({ weight: '400' })
 const poppinsBold = Poppins({ weight: '700' })
 
-const Anunt = () => {
+const Anunt = ({ anunt }) => {
     return (
         <div className={`${classes.anuntContainer} ${poppins.className}`}>
-            <h3 className={`${classes.anuntTitle} ${poppinsBold.className}`}>Licitatie de masa imbulzata</h3>
-            <p className={classes.anuntDescription}>OS Ciucas Scoate la licitatie o multitudine de masa imbulzata si este incredibil.</p>
+            <h3 className={`${classes.anuntTitle} ${poppinsBold.className}`}>{anunt.titlu}</h3>
+            <p className={classes.anuntDescription}>{anunt.descriere}</p>
             <div className={classes.anuntFooter}>
-                <p className={classes.anuntDate}>12.08.2022</p>
+                <p className={classes.anuntDate}>{anunt.data}</p>
                 <a target='_blank' rel='noreferrer' className={classes.anuntLink} href="https://www.osbucegipiatracraiuluiciucas.ro/wp-content/uploads/2022/10/Anunt-17.10.2022.pdf">Descarca</a>
             </div>
         </div>
--- components/anunturi/AnunturiBody.js
+++ components/anunturi/AnunturiBody.js
@@ -1,24 +1,47 @@
 import Link from "next/link"
 import { Poppins } from "@next/font/google"
+import { collection, getDocs } from "firebase/firestore"
+import { useEffect, useState } from "react"
 
 import classes from './AnunturiBody.module.css'
 import Anunt from "./Anunt"
+import { db } from "../../firebase"
 
 const poppins = Poppins({ weight: '400' })
 const poppinsBold = Poppins({ weight: '700' })
 
+// Try to render different types of announcements based on props coming in
 const AnunturiBody = (props) => {
+    const [anunturi, setAnunturi] = useState([])
+
+    useEffect(() => {
+        const getData = async () => {
+            const data = []
+            const querySnapshot = await getDocs(collection(db, "anunturi"));
+            querySnapshot.forEach((doc) => {
+                data.push(doc.data())
+            });
+
+            setAnunturi(data)
+        }
+
+        getData()
+    }, [])
+
     return (
         <section className={classes.container}>
             <h2 className={`${classes.title} ${poppinsBold.className}`}>Toate anunturile</h2>
 
             <div className={classes.anunturiContainer}>
+                {anunturi.map((doc) => {
+                    return <Anunt key={doc.titlu} anunt={doc} />
+                })}
+                {/* <Anunt />
                 <Anunt />
                 <Anunt />
                 <Anunt />
                 <Anunt />
-                <Anunt />
-                <Anunt />
+                <Anunt /> */}
             </div>
         </section>
     )
