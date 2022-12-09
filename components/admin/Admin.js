9ee7a2e3d18d7ea2a7fc835bec51ff7327eaf653
Gino Stoian <ginostoian@gmail.com>
2022-12-10 01:51:02 +0200

    add check to see if user is logged in when accessing admin page

---
+++ components/admin/Admin.js
@@ -0,0 +1,11 @@
+import classes from './Admin.module.css'
+
+const Admin = () => {
+    return (
+        <div>
+
+        </div>
+    )
+}
+
+export default Admin
---
+++ pages/admin/index.js
@@ -0,0 +1,45 @@
+import { useState, useEffect } from "react"
+import { useRouter } from "next/router"
+
+import HomeHero from "../../components/HomeHero"
+
+const AdminPage = () => {
+    const [loginStatus, setLoginStatus] = useState(null)
+    const router = useRouter()
+
+    const getWithExpiry = (key) => {
+        const itemStr = localStorage.getItem(key)
+        // if the item doesn't exist, return null
+        if (!itemStr) {
+            router.push('/admin/login')
+            return null
+        }
+        const item = JSON.parse(itemStr)
+        const now = new Date()
+        // compare the expiry time of the item with the current time
+        if (now.getTime() > item.expiry) {
+            // If the item is expired, delete the item from storage
+            // and return null
+            localStorage.removeItem(key)
+            router.push('/admin/login')
+            return null
+        }
+        return item.value
+    }
+
+    useEffect(() => {
+        setLoginStatus(getWithExpiry('isLoggedIn'))
+    })
+
+    console.log(loginStatus)
+
+    if (loginStatus === true) {
+        return (
+            <main>
+                <HomeHero title={'Panou de control'} height={'40vh'} />
+            </main>
+        )
+    }
+}
+
+export default AdminPage
