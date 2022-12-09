e9278489c54a2e612e95c85351ee1d5759563169
Gino Stoian <ginostoian@gmail.com>
2022-12-10 01:54:26 +0200

    refactor to solve useEffect issues

--- pages/admin/index.js
+++ pages/admin/index.js
@@ -7,29 +7,29 @@ const AdminPage = () => {
     const [loginStatus, setLoginStatus] = useState(null)
     const router = useRouter()
 
-    const getWithExpiry = (key) => {
-        const itemStr = localStorage.getItem(key)
-        // if the item doesn't exist, return null
-        if (!itemStr) {
-            router.push('/admin/login')
-            return null
-        }
-        const item = JSON.parse(itemStr)
-        const now = new Date()
-        // compare the expiry time of the item with the current time
-        if (now.getTime() > item.expiry) {
-            // If the item is expired, delete the item from storage
-            // and return null
-            localStorage.removeItem(key)
-            router.push('/admin/login')
-            return null
-        }
-        return item.value
-    }
-
     useEffect(() => {
+        const getWithExpiry = (key) => {
+            const itemStr = localStorage.getItem(key)
+            // if the item doesn't exist, return null
+            if (!itemStr) {
+                router.push('/admin/login')
+                return null
+            }
+            const item = JSON.parse(itemStr)
+            const now = new Date()
+            // compare the expiry time of the item with the current time
+            if (now.getTime() > item.expiry) {
+                // If the item is expired, delete the item from storage
+                // and return null
+                localStorage.removeItem(key)
+                router.push('/admin/login')
+                return null
+            }
+            return item.value
+        }
+
         setLoginStatus(getWithExpiry('isLoggedIn'))
-    })
+    }, [router])
 
     console.log(loginStatus)
 
