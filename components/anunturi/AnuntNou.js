acf90ce46f2ea66a9f607c86553e9d7d10e89f2e
Gino Stoian <ginostoian@gmail.com>
2022-12-18 22:56:25 +0200

    add new add component with markdown component

--- components/anunturi/AnuntNou.js
+++ components/anunturi/AnuntNou.js
@@ -1,8 +1,9 @@
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router";
 import { Poppins } from '@next/font/google'
+import dynamic from "next/dynamic";
 import "@uiw/react-md-editor/markdown-editor.css";
 import "@uiw/react-markdown-preview/markdown.css";
-import dynamic from "next/dynamic";
-import { useState } from "react";
 
 const MDEditor = dynamic(
     () => import("@uiw/react-md-editor").then((mod) => mod.default),
@@ -15,12 +16,41 @@ const poppins = Poppins({ weight: '400' })
 const poppinsBold = Poppins({ weight: '700' })
 
 const AnuntNou = () => {
-    const [value, setValue] = useState("**Hello world!!!**");
+    const [value, setValue] = useState("**Scrie textul anuntului aici folosind editorul**");
+    const [loginStatus, setLoginStatus] = useState(null)
+    const router = useRouter()
+
+    useEffect(() => {
+        const getWithExpiry = (key) => {
+            const itemStr = localStorage.getItem(key)
+            // if the item doesn't exist, return null
+            if (!itemStr) {
+                router.push('/admin/login')
+                return null
+            }
+            const item = JSON.parse(itemStr)
+            const now = new Date()
+            // compare the expiry time of the item with the current time
+            if (now.getTime() > item.expiry) {
+                // If the item is expired, delete the item from storage
+                // and return null
+                localStorage.removeItem(key)
+                router.push('/admin/login')
+                return null
+            }
+            return item.value
+        }
+
+        setLoginStatus(getWithExpiry('isLoggedIn'))
+    }, [router])
+
+    console.log(value)
 
     return (
         <div className={`${poppins.className} ${classes.container}`}>
             <div className={classes.editorContainer}>
                 <MDEditor value={value} onChange={setValue} height='400px' />
+                <button className={classes.postAdButton}>Posteaza Anunt</button>
             </div>
         </div>
     )
