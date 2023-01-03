9dbb53640307267a89823d67b705866a38ad2ecc
Gino Stoian <ginostoian@gmail.com>
2023-01-04 01:54:52 +0200

    modify route used when pressing admin dashboard button

--- components/Footer.js
+++ components/Footer.js
@@ -10,7 +10,7 @@ const Footer = () => {
 
     const handleAdminClick = (e) => {
         e.preventDefault()
-        router.push('/admin/login')
+        router.push('/admin')
     }
 
     return (
