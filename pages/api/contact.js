2f74698f774bb286c4d458a0448d8c68561a1214
Gino Stoian <ginostoian@gmail.com>
2023-01-14 23:54:14 +0200

    create api route to send emails from contact form via nodemailer

--- pages/api/contact.js
+++ pages/api/contact.js
@@ -1,5 +1,33 @@
 // eslint-disable-next-line import/no-anonymous-default-export
 export default function (req, res) {
+    const nodeMailer = require('nodeMailer')
+    const transporter = nodeMailer.createTransport({
+        port: 465,
+        host: "smtp.gmail.com",
+        auth: {
+            user: 'osciucas.alerts@gmail.com',
+            pass: 'jbjyltbgaeywmwux',
+        },
+        secure: true,
+    })
+
+    const mailData = {
+        from: 'osciucas.alerts@gmail.com',
+        to: 'ginostoian@gmail.com',
+        subject: `Mesaj nou de la ${req.body.email}`,
+        text: req.body.message,
+        html: `<div>${req.body.message}</div>`
+    }
+
+    transporter.sendMail(mailData, (err, info) => {
+        if (err) {
+            console.log(err)
+        }
+        if (info) {
+            console.log(info)
+        }
+    })
+
     console.log(req.body)
     res.send(200)
 }
